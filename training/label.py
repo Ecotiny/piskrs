fcc7d8883190f3e9abff0d486a64582772ae0d47
Linus Molteno <linus@molteno.net>
2021-08-02 13:32:42 +1200

    Training skeleton

---
+++ training/label.py
@@ -0,0 +1,3 @@
+# check to see which data is already labelled and what isn't labelled
+# open up an OpenCV interface for labelling the data, scrolling to the selection frame, 
+# and then type "p", "s", or "r" depending on what it is for each half of the frame
